e246de040ad42c4e24629bc5fe495a08723b9ccd
jerry <jerry@jerry.com>
2021-09-28 01:35:37 +0200

    support -i flag

--- wc.py
+++ wc.py
@@ -5,6 +5,16 @@ try:
 
     # counts the number of words of a string passed in as a
     # command-line argument, excluding spaces and ignoring case
+
+    # adding a new function, which also counts the numbers of
+    # chars
+
+    # allow for directory-names as input arguments - in this case,
+    # the word count is to be applied to all text-files of that directory
+
+    # Add the capability to support command-line options to wc.py (e.g. the
+    # option '-i' could be used to support case-insensitivity for counting words)
     print('Number of words (excluding spaces):', len(string_argument.split()))
+    print('Number of chars (including spaces):', len(string_argument))
 except IndexError:
     print('Provide an argument please...')
