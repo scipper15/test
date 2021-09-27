41bbc36cbd0f4113ee0dcc07bfe15614f37ae505
jerry <jerry@jerry.com>
2021-09-28 01:17:15 +0200

    # counts the number of words of a string passed in as a     # command-line argument, excluding spaces and ignoring case

--- wc.py
+++ wc.py
@@ -3,6 +3,8 @@ import sys
 try:
     string_argument = sys.argv[1]
 
+    # counts the number of words of a string passed in as a
+    # command-line argument, excluding spaces and ignoring case
     print('Number of words (excluding spaces):', len(string_argument.split()))
 except IndexError:
     print('Provide an argument please...')
