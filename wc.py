050970fa6c8d95ceec7f98590fd420e54fd085b3
tom <eichhorn@wbs.de>
2021-09-28 01:29:56 +0200

    Allow for directory names

--- wc.py
+++ wc.py
@@ -5,8 +5,12 @@ try:
 
     # counts the number of words of a string passed in as a
     # command-line argument, excluding spaces and ignoring case
+
     # adding a new function, which also counts the numbers of
     # chars
+
+    # allow for directory-names as input arguments - in this case,
+    # the word count is to be applied to all text-files of that directory
     print('Number of words (excluding spaces):', len(string_argument.split()))
     print('Number of chars (including spaces):', len(string_argument))
 except IndexError:
